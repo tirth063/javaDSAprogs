ef50eab4d4d667cf4f9b50af56956bcce4914e10
Tirth Patel <122536331+tirth063@users.noreply.github.com>
2024-06-03 23:30:56 +0530

    tree in pre post order traversal of binary tree without recusion code is done.

--- DSAs/treedsaq/Tree.java
+++ DSAs/treedsaq/Tree.java
@@ -1,240 +1,230 @@
 package DSAs.treedsaq;
-
 import java.util.LinkedList;
 import java.util.Queue;
-class BinaryTree {
-    public static int countNodes(Node1 root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftCount = countNodes(root.left);
-        int rightCount = countNodes(root.right);
-        return leftCount + rightCount + 1;
-    }
 
-    public static int height(Node1 root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
-
-    public static int countLeafNodes(Node1 root) {
-        if (root == null) {
-            return 0;
-        }
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-        int leftCount = countLeafNodes(root.left);
-        int rightCount = countLeafNodes(root.right);
-        return leftCount + rightCount;
-    }
-
+public class Tree {
     static int count = 0;
 
-    public static void findNthNode(Node1 root, int n) {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = new Node(10);
+        binaryTree.root.left = new Node(20);
+        binaryTree.root.right = new Node(30);
+        binaryTree.root.left.left = new Node(40);
+        binaryTree.root.left.right = new Node(50);
+        binaryTree.root.right.left = new Node(60);
+        binaryTree.root.right.right = new Node(70);
+        binaryTree.root.left.left.left = new Node(80);
+        binaryTree.root.right.right.right = new Node(100);
+
+        BinaryTree binaryTree1 = new BinaryTree();
+        binaryTree1.root = new Node(10);
+        binaryTree1.root.left = new Node(20);
+        binaryTree1.root.right = new Node(30);
+
+        System.out.println("level order: ");
+        binaryTree.levelOrder(binaryTree.root);
+        System.out.println();
+
+
+        System.out.println("pre order: ");
+        binaryTree.printPreOrder(binaryTree.root);
+        System.out.println();
+
+
+        System.out.println("in order: ");
+        binaryTree.printInOrder(binaryTree.root);
+        System.out.println();
+
+
+        System.out.println("post order: ");
+        binaryTree.printPostOrder(binaryTree.root);
+        System.out.println();
+
+        System.out.println("count nodes: ");
+        System.out.println(binaryTree.countNodes(binaryTree.root));
+
+        System.out.println("find Nth node: ");
+        findNthNode(binaryTree.root, 3);
+        System.out.println();
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        Node node = binaryTree.createTreeFromArray(arr);
+        System.out.println(node.data);
+
+        binaryTree.levelOrder(node);
+        System.out.println();
+
+        binaryTree.printPreOrder(node);
+        System.out.println();
+
+        binaryTree.printInOrder(node);
+        System.out.println();
+
+        binaryTree.printPostOrder(node);
+
+        System.out.println();
+        System.out.println( binaryTree.isSameTree(binaryTree.root, binaryTree1.root));
+
+        System.out.println(binaryTree.isSameTreeQueue(binaryTree.root, binaryTree1.root));
+
+        System.out.println(findNthElement(binaryTree.root, 20));
+    }
+
+    public static void findNthNode(Node root, int n) {
         if (root == null) {
             return;
         }
         findNthNode(root.left, n);
         count++;
         if (count == n) {
-            System.out.println(root.data + " ");
+            System.out.print(root.data);
             return;
         }
         findNthNode(root.right, n);
     }
 
-    public static boolean isPresent(Node1 root, int data) {
+    public static boolean findNthElement(Node root, int n) {
         if (root == null) {
             return false;
         }
-        if (root.data == data) {
+
+        if (root.data == n) {
             return true;
         }
-        return isPresent(root.left, data) || isPresent(root.right, data);
+
+        return  findNthElement(root.left, n) || findNthElement(root.right, n);
     }
 
-    public static boolean isBalanced(Node1 root) {
-        if (root == null) {
-            return true;
-        }
-        if (isBalanced(root.left) == false) {
-            return false;
-        }
-        if (isBalanced(root.right) == false) {
-            return false;
-        }
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        if (Math.abs(leftHeight - rightHeight) <= 1) {
-            return true;
-        }
-        return false;
-    }
+}
 
-    //    Find the Maximum Depth /  Height of given Binary Tree
-    public static int maxDepth(Node1 root) {
-        if (root == null) {
-            return 0;
-        }
+class BinaryTree {
+    Node root;
 
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    }
-
-
-    // Create binary tree from an array - level order fashion
-    public static Node1 createTreeFromArray(int[] arr) {
+    public Node createTreeFromArray(int[] arr) {
         if (arr == null || arr.length == 0) {
             return null;
         }
 
-        Node1 root = new Node1(arr[0]);
-        Queue<Node1> queue = new LinkedList<>();
-        queue.add(root);
+        Node root = new Node(arr[0]);
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
         int i = 1;
-        while (!queue.isEmpty() && i < arr.length) {
-            Node1 curr = queue.remove();
-            if (i < arr.length && arr[i] != -1) {
-                curr.left = new Node1(arr[i]);
-                queue.add(curr.left);
+        while (i < arr.length) {
+            Node curr = q.remove();
+
+            curr.left = new Node(arr[i++]);
+            q.add(curr.left);
+
+            if (i < arr.length) {
+                curr.right = new Node(arr[i++]);
+                q.add(curr.right);
             }
-            i++;
-            if (i < arr.length && arr[i] != -1) {
-                curr.right = new Node1(arr[i]);
-                queue.add(curr.right);
-            }
-            i++;
         }
         return root;
     }
 
-    public static Node1 sortedArrayToBST(int[] nums) {
-        int mid = (1 + nums.length) / 2;
-        int left  = 0;
-        int right = mid+1;
-
-        Node1 root = new Node1(nums[mid]);
-        Queue<Node1> queue = new LinkedList<>();
-        queue.add(root);
-        int i = 0;
-        while (!queue.isEmpty() && i < mid) {
-            Node1 curr = queue.remove();
-            if (i < mid && left < mid ) {
-
-                curr.left = new Node1(nums[left]);
-                queue.add(curr.left);
-            }
-            left++;
-            i++;
-            if (i < mid && right < nums.length ) {
-                curr.right = new Node1(nums[right]);
-                queue.add(curr.right);
-            }
-            right++;
-            i++;
-        }
-        return root;
-    }
-
-    static void levelOrder(Node1 root) {
+    public void levelOrder(Node root) {
         if (root == null) {
             return;
         }
-        System.out.print( root.data + " ");
-        levelOrder(root.left);
-        levelOrder(root.right);
-    }
-    public static void main(String[] args) {
-        Tree tree = new Tree();
-
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        Node1 root = sortedArrayToBST(arr);
-
-        levelOrder(root);
-    }
-}
-
-public class Tree {
-    Node1 root;
-
-    public void insert(int data){
-        root = insert(root,data);
-    }
-
-    public Node1 insert(Node1 root, int data){
-        if(root == null){
-            root = new Node1(data);
-            return root;
-        }
-        if(data < root.data){
-            root.left = insert(root.left,data);
-        }else if(data > root.data){
-            root.right = insert(root.right,data);
-        }
-        return root;
-    }
-
-    //level order BFS traversal
-    public void levelOrder(){
-        if(root == null){
-            return;
-        }
-        Queue<Node1> queue = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
         queue.add(root);
-        while(!queue.isEmpty()){
-            Node1 temp = queue.poll();
-            System.out.print(temp.data+" ");
-            if(temp.left != null){
-                queue.add(temp.left);
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
+            if (n == null) {
+                return;
             }
-            if(temp.right != null){
-                queue.add(temp.right);
+            System.out.print(n.data + " ");
+            if (n.left != null) {
+                queue.add(n.left);
+            }
+            if (n.right != null) {
+                queue.add(n.right);
             }
         }
     }
 
-    //pre, in, post order traversal
-    public void printPreOrder(Node1 root){
-        if(root == null){
+    public void printPreOrder(Node root) {
+        if (root == null) {
             return;
         }
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         printPreOrder(root.left);
         printPreOrder(root.right);
     }
 
-    public void printInOrder(Node1 root){
-        if(root == null){
+    public void printInOrder(Node root) {
+        if (root == null) {
             return;
         }
         printInOrder(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         printInOrder(root.right);
     }
 
-    public void printPostOrder(Node1 root){
-        if(root == null){
+    public void printPostOrder(Node root) {
+        if (root == null) {
             return;
         }
         printPostOrder(root.left);
         printPostOrder(root.right);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
     }
 
+    public int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftCount = countNodes(root.left);
+        int rightCount = countNodes(root.right);
+
+        return leftCount + rightCount + 1;
+    }
+
+    public boolean isSameTree(Node p, Node q){
+        if(p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null || p.data != q.data){
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public boolean isSameTreeQueue(Node p, Node q){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+
+        while (!queue.isEmpty()){
+            Node f = queue.poll();
+            Node s = queue.poll();
+
+            if (f == null && s == null){
+                continue;
+            } else if (f == null || s == null || f.data != s.data) {
+                return false;
+            }
+
+            queue.add(f.left);
+            queue.add(s.left);
+            queue.add(f.right);
+            queue.add(s.right);
+        }
+
+        return true;
+    }
 
 }
 
-class Node1 {
+class Node {
     int data;
-    Node1 left;
-    Node1 right;
-    Node1(int data){
+    Node left, right;
+
+    Node(int data) {
         this.data = data;
-        left = null;
-        right = null;
     }
 }
--- DSAs/treedsaq/TreetravelsalWithoutUsingRecursion.java
+++ DSAs/treedsaq/TreetravelsalWithoutUsingRecursion.java
@@ -1,2 +1,149 @@
-package DSAs.treedsaq;public class TreetravelsalWithoutUsingRecursion {
+package DSAs.treedsaq;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class TreetravelsalWithoutUsingRecursion {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    Node root;
+
+    TreetravelsalWithoutUsingRecursion() {
+        root = null;
+    }
+
+    void levelOrder() {
+        if (root == null)
+            return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()){
+            Node n = q.poll();
+            System.out.print(n.data + " ");
+            if (n.left != null)
+                q.add(n.left);
+            if (n.right != null)
+                q.add(n.right);
+        }
+    }
+
+    void preOrder(){
+        if (root == null)
+            return;
+
+        Stack<Node> s = new Stack<>();
+        s.add(root);
+
+        while (!s.isEmpty()){
+            Node n = s.pop();
+            System.out.print(n.data + " ");
+            if (n.right != null)
+                s.add(n.right);
+            if (n.left != null)
+                s.add(n.left);
+
+        }
+    }
+
+    private void postOrder() {
+        if (root == null)
+            return;
+
+        Stack<Node> s = new Stack<>();
+        Stack<Node> t = new Stack<>();
+        s.add(root);
+
+        while (!s.isEmpty()){
+             t.add( s.pop());
+
+            if (t.peek().left != null)
+                s.add(t.peek().left);
+            if (t.peek().right != null)
+                s.add(t.peek().right);
+
+
+        }
+        while (!t.isEmpty()){
+            System.out.print(" " + t.pop().data);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreetravelsalWithoutUsingRecursion tree = new TreetravelsalWithoutUsingRecursion();
+        tree.root = new Node(10);
+        tree.root.left = new Node(20);
+        tree.root.right = new Node(30);
+        tree.root.left.left = new Node(40);
+        tree.root.left.right = new Node(50);
+        tree.root.right.left = new Node(60);
+        tree.root.right.right = new Node(70);
+        tree.root.left.left.left = new Node(80);
+        tree.root.right.right.right = new Node(90);
+        tree.root.left.right.left = new Node(100);
+        tree.root.left.right.right = new Node(110);
+        tree.root.right.left.left = new Node(120);
+        tree.root.right.left.right = new Node(130);
+        tree.root.right.right.left = new Node(140);
+        tree.root.right.right.right = new Node(150);
+
+
+
+        System.out.println("Level order traversal of binary tree is:");
+        tree.levelOrder();
+
+        System.out.println("\n pre order traversal of binary tree is: ");
+        tree.preOrder();
+
+        System.out.println("\n post order traversal of binary tree is:");
+        tree.postOrder();
+
+        System.out.println("\n in order traversal of binary tree is:");
+        tree.inOrder();
+    }
+    /*
+    *                                       10
+    *
+    *
+    *                   20                                      30
+    *
+    *
+    *         40                  50                  60                  70
+    *
+    *
+    *   80          90      100         110     120         130     140         150
+    *
+    *
+    *
+    * */
+    private void inOrder() {
+        if (root == null)
+            return;
+
+        Stack<Node> s = new Stack<>();
+        Node c = root;
+        while (c != null || s.size() > 0){
+            while (c != null) {
+                s.push(c);
+                c = c.left;
+
+            }
+            c = s.pop();
+            System.out.print(c.data + " ");
+            c = c.right;
+        }
+    }
+
+
 }
